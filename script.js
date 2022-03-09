cfb59510cc6962ca3498bc92039095caacab944e
Anna <usune@mail.ru>
2022-03-09 04:02:12 +0300

    mmmmmmm

--- script.js
+++ script.js
@@ -2,18 +2,32 @@
 
 const film = document.getElementById("film");
 const res = document.querySelector(".res");
+var selectElem = document.getElementById("select");
 
 const getData = (url) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      let moviesArr = {};
+      let removeDublicate;
+      data.forEach(function (item) {
+        [].concat(item.movies).forEach(function (item) {
+          if (item !== undefined) {
+            moviesArr[item] = true;
+          }
+        });
+        removeDublicate = Object.keys(moviesArr);
+      });
+      console.log(moviesArr);
+      console.log(removeDublicate);
+
       let optionAll = document.createElement("option");
       optionAll.innerHTML = "All";
       film.append(optionAll);
-      data.forEach(function (item) {
+      removeDublicate.forEach(function (item) {
         let option = document.createElement("option");
-        option.innerHTML = item.name;
-        option.value = item.name;
+        option.innerHTML = item;
+        option.value = item;
         film.append(option);
       });
 
@@ -23,6 +37,7 @@ const getData = (url) => {
           let divCard = document.createElement("div");
           divCard.className = "div_card";
           res.append(divCard);
+
           for (let key in item) {
             let newSelect = JSON.parse(JSON.stringify(item), (key, value) =>
               key === "photo" ? "<img src= " + item.photo + " >" : value
@@ -37,21 +52,39 @@ const getData = (url) => {
       all();
       const result = function () {
         res.innerHTML = "";
-        let select = data[event.target.options.selectedIndex - 1];
-        let divCard = document.createElement("div");
-        divCard.className = "div_card";
-        res.append(divCard);
-        for (let key in select) {
-          let newSelect = JSON.parse(JSON.stringify(select), (key, value) =>
-            key === "photo" ? "<img src= " + select.photo + " >" : value
-          );
+        let select = film.value;
 
-          let div = document.createElement("div");
-          div.id = "div";
-          div.innerHTML = "<b>" + key + "</b>" + ": " + newSelect[key];
-          divCard.append(div);
-        }
-        if (event.target.options.selectedIndex == 0) {
+        const newData = data.filter(function (val) {
+          return !(
+            val.movies === "" ||
+            typeof val.movies == "undefined" ||
+            val.movies === null
+          );
+        });
+
+        newData.forEach(function (item, index) {
+          item["movies"].forEach(function (item2) {
+            if (item2 == select) {
+              console.log(index);
+
+              let divCard = document.createElement("div");
+              divCard.className = "div_card";
+              res.append(divCard);
+
+              for (let key in item) {
+                let newSelect = JSON.parse(JSON.stringify(item), (key, value) =>
+                  key === "photo" ? "<img src= " + item.photo + " >" : value
+                );
+                let div = document.createElement("div");
+                div.id = "div";
+                div.innerHTML = "<b>" + key + "</b>" + ": " + newSelect[key];
+                divCard.append(div);
+              }
+            }
+          });
+        });
+
+        if (film.selectedIndex == 0) {
           all();
         }
       };
